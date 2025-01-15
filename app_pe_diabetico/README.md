cf0178656a9d42443528b182fc653968e51a60d2
Ruben Gananca <ruben-gananca@live.com.pt>
2025-01-15 11:27:05 +0000

    Salvando alterações da pasta app_pe_diabetico

---
+++ app_pe_diabetico/README.md
@@ -0,0 +1,4 @@
+# app_pe_diabetico
+
+Informative Mobile Application for Monitoring and Preventing Diabetic Foot Disease
+
---
+++ app_pe_diabetico/lib/galery.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// TODO: Quando a aplicação é encerrada a galeria volta a ficar a 0. Guardar o estado da mesma
+
+class Galery extends StatelessWidget {
+  const Galery({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Recupera os argumentos enviados pela página camera.dart
+    final arguments = ModalRoute.of(context)!.settings.arguments as Map<File,String>; // Interpreta como Map
+
+    return Scaffold(
+        backgroundColor: Color(0xFFEAEAEA),
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          title: Text("Galeria",
+            style: GoogleFonts.nunito(
+              fontSize: 25,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: arguments.isEmpty ?
+        // Se a galeria estiver vazia
+        const Center(
+          child: Text("Nenhuma imagem capturada ainda"),
+        )
+            : GridView.builder( // se nao estiver vazio mostra a galeria
+            padding: const EdgeInsets.all(8.0),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // numero de colunas
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+            ),
+            itemCount: arguments.length,
+            itemBuilder: (context, index) {
+              final file = arguments.keys.elementAt(index);
+              final date = arguments[file]!;
+
+              return Column(
+                children: [
+                  // Para a imagem ocupar mais espaço da coluna
+                  Expanded(
+                    child: Image.file(file, fit: BoxFit.cover),
+                  ),
+                  const SizedBox(height: 5.0),
+                  Text(
+                    date,
+                    style: GoogleFonts.nunito(fontSize: 12.0),
+                  ),
+                ],
+              );
+            }
+        )
+    );
+  }
+}
